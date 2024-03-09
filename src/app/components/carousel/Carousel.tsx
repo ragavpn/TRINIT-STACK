8c92d14db5b7a0fd2fb46fabeaccef85b5047fd1
Elegant Shock <elegantShock3307@gmail.com>
2024-03-09 11:09:25 +0530

    i got aa carousel :D

---
+++ src/app/components/carousel/Carousel.tsx
@@ -0,0 +1,166 @@
+"use client";
+
+import React, { FC, useState, CSSProperties } from "react";
+import "./carousel.css";
+import Image from "next/image";
+
+interface CarouselInfo {
+  images: string[];
+  setFocusElement?: Function;
+  offsetAngle?: number;
+  carouselRadius?: number;
+  centralImageRadius?: number;
+  centralImageBoxShadow?: string;
+  peripheralImageRadius?: number;
+  peripheralImageBoxShadow?: string;
+  focusElementStyling?: CSSProperties;
+  border?: boolean;
+  borderWidth?: number;
+  borderHexColor?: string;
+  autoRotateTime?: number;
+  transitionTime?: number;
+  navigationTextSize?: number;
+  navigationButtonRadius?: number;
+  navigationButtonBgColor?: string;
+  navigationButtonColor?: string;
+  navigationButtonStyling?: CSSProperties;
+  carousel: any;
+  setCarousel: Function;
+}
+
+export const FancyCarousel: FC<CarouselInfo> = ({
+  images,
+  setFocusElement = () => {},
+  offsetAngle = 0,
+  carouselRadius = 550,
+  centralImageRadius = 0,
+  centralImageBoxShadow = "",
+  peripheralImageRadius = 75,
+  peripheralImageBoxShadow = "",
+  focusElementStyling = {},
+  border = false,
+  borderWidth = 5,
+  borderHexColor = "CB786C",
+  autoRotateTime = 0,
+  transitionTime = 1.5,
+  navigationTextSize = 2,
+  navigationButtonRadius = 32.5,
+  navigationButtonBgColor = "CB786C",
+  navigationButtonColor = "FFFFFF",
+  navigationButtonStyling = {},
+  carousel = [],
+  setCarousel = () => {},
+}: CarouselInfo) => {
+  setFocusElement(carousel.focusElement);
+
+  const noOfImages: number = images.length;
+  const theta: number = 360 / noOfImages;
+
+  const borderElement: string = border
+    ? `url("data:image/svg+xml,%3csvg width=\'100%25\' height=\'100%25\' xmlns=\'http://www.w3.org/2000/svg\'%3e%3crect width=\'100%25\' height=\'100%25\' fill=\'none\' rx=\'${carouselRadius * 2}\' ry=\'${carouselRadius * 2}\' stroke=\'%23${borderHexColor}FF\' stroke-width=\'${borderWidth}\' stroke-dasharray=\'6%2c 24\' stroke-dashoffset=\'0\' stroke-linecap=\'square\'/%3e%3c/svg%3e")`
+    : "";
+
+  var newCoordinates: number[][] = [];
+  images.forEach((item, index) => {
+    newCoordinates.push([
+      carouselRadius -
+        peripheralImageRadius +
+        carouselRadius * Math.cos((2 * Math.PI * index) / noOfImages),
+      carouselRadius -
+        peripheralImageRadius +
+        carouselRadius * Math.sin((2 * Math.PI * index) / noOfImages),
+    ]);
+  });
+
+  // rotate point around (carouselRadius-peripheralRadius, carouselRadius-peripheralRadius) by (offset+90) deg
+  const totalDeviation: number = (offsetAngle * Math.PI) / 180 + Math.PI / 2;
+
+  var rotatedCoordinates: number[][] = [];
+  const centerCoordinate: number = carouselRadius - peripheralImageRadius;
+  newCoordinates.forEach((item, index) => {
+    rotatedCoordinates.push([
+      centerCoordinate +
+        (item[0] - centerCoordinate) * Math.cos(totalDeviation) -
+        (item[1] - centerCoordinate) * Math.sin(totalDeviation),
+      centerCoordinate +
+        (item[0] - centerCoordinate) * Math.sin(totalDeviation) +
+        (item[1] - centerCoordinate) * Math.cos(totalDeviation),
+    ]);
+  });
+  return (
+    <div className="fancy-carousel-wrapper-element">
+      <div
+        className="fancy-carousel-border"
+        style={{
+          backgroundImage: borderElement,
+          height: `${carouselRadius * 2}px`,
+          width: `${carouselRadius * 2}px`,
+          transition: `${transitionTime}`,
+        }}
+      >
+        <div
+          className="fancy-carousel"
+          style={{
+            transform: `rotate(${carousel.carouselOrietation}deg)`,
+            height: `${carouselRadius * 2}px`,
+            width: `${carouselRadius * 2}px`,
+          }}
+        >
+          {images.map((item, index) =>
+            index !== carousel.focusElement ? (
+              <div
+                className="fancy-carousel-element"
+                key={index}
+                style={{
+                  transform: `rotate(${carousel.elementOrientation}deg)`,
+                  width: `${peripheralImageRadius * 2}px`,
+                  height: `${peripheralImageRadius * 2}px`,
+                  left: `${rotatedCoordinates[index][0]}px`,
+                  bottom: `${rotatedCoordinates[index][1]}px`,
+                  boxShadow: `${peripheralImageBoxShadow}`,
+                  transition: `${transitionTime}`,
+                }}
+              >
+                <Image
+                  className="fancy-carousel-image"
+                  src={item}
+                  width={`${peripheralImageRadius * 2}`}
+                  height={`${peripheralImageRadius * 2}`}
+                  alt={item}
+                />
+              </div>
+            ) : (
+              <div
+                className="fancy-carousel-element"
+                key={index}
+                style={{
+                  ...{
+                    transform: `rotate(${carousel.elementOrientation}deg)`,
+                    width: `${peripheralImageRadius * 2}px`,
+                    height: `${peripheralImageRadius * 2}px`,
+                    left: `${rotatedCoordinates[index][0]}px`,
+                    bottom: `${rotatedCoordinates[index][1]}px`,
+                    boxShadow: `${peripheralImageBoxShadow}`,
+                    transition: `${transitionTime}`,
+                  },
+                  ...focusElementStyling,
+                }}
+              >
+                <Image
+                  className="fancy-carousel-image"
+                  src={item}
+                  style={{
+                    transition: `${transitionTime}`,
+                  }}
+                  width={`${peripheralImageRadius * 2}`}
+                  height={`${peripheralImageRadius * 2}`}
+                  alt={item}
+                />
+              </div>
+            ),
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import "./PageStyles.css";
 import { Blob } from "../components/Blob/Blob";
 
@@ -6,9 +7,35 @@ import blob1 from "../../assets/blob1.svg";
 import blob2 from "../../assets/blob2.svg";
 import blob3 from "../../assets/blob3.svg";
 import blob4 from "../../assets/blob4.svg";
+import { FancyCarousel } from "../components/carousel/Carousel";
 
 export default function Home() {
   const blobs = [blob1, blob2, blob3, blob4, blob1];
+  const [carousel, setCarousel] = useState({
+    carouselOrietation: 0,
+    elementOrientation: 0,
+    focusElement: 0,
+  });
+  const rotateRight = () => {
+    setCarousel({
+      carouselOrietation: carousel.carouselOrietation + theta,
+      elementOrientation: carousel.elementOrientation - theta,
+      focusElement:
+        carousel.focusElement < noOfImages - 1 ? carousel.focusElement + 1 : 0,
+    });
+  };
+
+  const rotateLeft = () => {
+    setCarousel({
+      carouselOrietation: carousel.carouselOrietation - theta,
+      elementOrientation: carousel.elementOrientation + theta,
+      focusElement:
+        carousel.focusElement > 0 ? carousel.focusElement - 1 : noOfImages - 1,
+    });
+  };
+
+  const noOfImages: number = 5;
+  const theta: number = 360 / noOfImages;
   return (
     <div className="back">
       <div className="blobContainer">
@@ -23,7 +50,29 @@ export default function Home() {
           );
         })}
       </div>
-      <div className="foreground"></div>
+      <div className="foreground">
+        <div className="carousel">
+          <FancyCarousel
+            images={Array(5).fill("/vercel.svg")}
+            carousel={carousel}
+            setCarousel={setCarousel}
+          />
+        </div>
+        <button
+          onClick={() => {
+            rotateLeft();
+          }}
+        >
+          d
+        </button>
+        <button
+          onClick={() => {
+            rotateRight();
+          }}
+        >
+          e
+        </button>
+      </div>
     </div>
   );
 }
